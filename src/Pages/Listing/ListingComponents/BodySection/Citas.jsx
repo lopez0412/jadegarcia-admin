34598fe9bf7602d0b35c8db3fedff8ac118950e8
Alejandro Lopez <lopez.javier0412@gmail.com>
2024-07-02 20:06:10 -0600

    Add functionality to appointments by users,

--- src/Pages/Listing/ListingComponents/BodySection/Citas.jsx
+++ src/Pages/Listing/ListingComponents/BodySection/Citas.jsx
@@ -18,6 +18,7 @@ import { FiPhoneCall } from "react-icons/fi";
 import { GrCatalog } from "react-icons/gr";
 import ListingActions from '../ListingActions/ListingActions';
 import Select from 'react-select'
+import { CiHeart } from "react-icons/ci";
 
 
 
@@ -31,6 +32,7 @@ function Citas() {
   const baseUrl ='http://localhost:3000/api'
   const [reservaciones, setReservaciones] = useState([]);
   const [personal, setPersonal] = useState([]);
+  const [showNotes, setShowNotes] = useState(false);
   const [horarios, setHorarios] = useState([]);
   const [decoded, setDecoded] = useState('')
   const [loading, setLoading] = useState(false);
@@ -41,6 +43,7 @@ function Citas() {
   const [suggestions, setSuggestions] = useState([]);
   const [newDate, setNewDate] = useState('')
   const [serviceId, setServiceId] = useState('')
+  const [newNote, setNewNote] = useState('')
 
   const navigate = useNavigate();
 
@@ -260,6 +263,41 @@ const getAppoinments = async () => {
           }
       })
   }
+
+  const handleShowNotes = () =>{
+    setShowNotes(true)
+  }
+
+  const handleHideNotes = () =>{
+    setShowNotes(false)
+  }
+
+  const handleAddNote = async() =>{
+    try {
+      const response = await axios.post(`${baseUrl}/addUserNote`, {
+        userId: selectedEvent.data.userId._id,
+        note: newNote
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'auth-token': token
+        }
+      });
+      if (response.status === 200) {
+        show_alerta('Nota actualizada con éxito', 'success');
+        setNewNote(''); // Clear the new note field after successful submission
+        window.location.reload();
+      } else {
+        throw new Error('Failed to update note');
+      }
+    } catch (error) {
+      console.error('Error adding note:', error);
+      show_alerta('Error al agregar la nota', 'error');
+    }
+  };
+  
+
+ 
   return (
     <div className="RightDiv grid">
     <div className="listingContainer">
@@ -284,6 +322,36 @@ const getAppoinments = async () => {
     </div>
    </div>
    <div className={details}  style={{ zIndex: 11}}>
+
+            {showNotes ? (
+              <div style={{ position: 'fixed', top: '20%', left: '30%', width: '40%', height: '60%', backgroundColor: 'white', padding: '20px', overflowY: 'scroll', zIndex: 1000, border: '1px solid black' }}>
+               <span className="cardTitle flex">
+               Notas{" "}
+                <AiOutlineCloseCircle className="icon" onClick={() => handleHideNotes()} />
+              </span>
+              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: '10px' }}>
+              <textarea
+                placeholder="Escribe una nueva nota aquí..."
+                value={newNote}
+                onChange={(e) => setNewNote(e.target.value)}
+                required
+                style={{ width: '100%', padding: '10px', borderRadius: '10px', border: '1px solid #ccc' }}
+              />
+              <button type="submit" className="btn" onClick={handleAddNote} style={{ padding: '10px 20px', borderRadius: '20px', backgroundColor: 'var(--primaryColor)', color: 'white', border: 'none', cursor: 'pointer' }}>Agregar Nota</button>
+              </div>
+
+              <div style={{marginTop: '20px'}}>
+                {selectedEvent.data.userId.informacion.notas.map((note, index) => (
+                  <div className='flex'>
+                  <CiHeart /><p key={index}>{note}</p>
+                  </div>
+                ))}
+              </div>
+              
+            
+             
+            </div>
+            ) : <div></div>}
     
         {selectedEvent ? (
         <div>
@@ -298,8 +366,8 @@ const getAppoinments = async () => {
               <div className="top flex">
                 <span className="bio">
                   <span className="name">
-                    {selectedEvent.data.nombre}
-                    <small>{selectedEvent.data.fechaNacimiento.split("T")[0]}</small>
+                    {selectedEvent.data.userId ? selectedEvent.data.userId.informacion.nombre : selectedEvent.data.nombre} ({selectedEvent.data.userId ? "Usuario" : "Cliente"})
+                    <small>{selectedEvent.data.userId ? selectedEvent.data.userId.informacion.fechaNacimiento.split("T")[0] : selectedEvent.data.fechaNacimiento.split("T")[0]}</small>
                   </span>
                   
                 </span>
@@ -307,11 +375,11 @@ const getAppoinments = async () => {
 
               <div className="bottom">
                 <p>
-                  {selectedEvent.data.preferencias}
+                  {selectedEvent.data.userId ? selectedEvent.data.userId.informacion.preferencias : selectedEvent.data.preferencias}
                 </p>
                 <div className="details grid">
                   <span>
-                    Telefono: <small>{selectedEvent.data.telefono}</small>
+                    Telefono: <small>{selectedEvent.data.userId ? selectedEvent.data.userId.informacion.telefono : selectedEvent.data.telefono}</small>
                   </span>
                   <span>
                     Fecha Cita: <small>{selectedEvent.data.citaDate.split("T")[0]}</small>
@@ -322,6 +390,11 @@ const getAppoinments = async () => {
                   <span>
                     Estado: <small>{selectedEvent.data.estado} </small>
                   </span>
+                  {selectedEvent.data.userId && (
+                    <span>
+                      <button className='btn' id='verNotes' onClick={() =>handleShowNotes()}>Ver notas</button>
+                    </span>
+                  )}
                 </div>
                 <div className="contact flex">
                   <span>Contacta Cliente:</span>
@@ -331,6 +404,7 @@ const getAppoinments = async () => {
                     </span>
                   </div>
                 </div>
+               
                <div>
                 <ListingActions cita={selectedEvent} removeDetalles={removeDetails} getApp={getAppoinments}/>
                 </div>
@@ -371,8 +445,11 @@ const getAppoinments = async () => {
                 {selectedEvent.data.servicioId.descripcion}
                 </p>
               </span>
+             
               </div>
+              
             </div>
+           
           
               
         </div>) : (<div>
